1986ffda2f58a1be3a6fed43bba58f237f351be9
lsdlinshunda <923076444@qq.com>
2018-01-22 17:23:39 +0800

    增加年份数据的标注

--- transformer/txt2Json.js
+++ transformer/txt2Json.js
@@ -1,8 +1,9 @@
 const readline = require("readline")
 const fs = require("fs")
 
-const rl = readline.createInterface({
-    input: fs.createReadStream("network.txt")
+//读取结点出现年份
+const rl_year = readline.createInterface({
+    input: fs.createReadStream("year.txt")
 });
 
 var i = 1
@@ -11,18 +12,40 @@ var result = {
     "links": [],
 }
 
-rl.on('line', (line) => {
-    // console.log('Line from file:' + i + " " + line)
-    // i += 1
+var year = {}
+
+//先读取年份
+rl_year.on('line', (line) => {
     var arr = line.split("\t")
-    result.nodes.push({"id": arr[0], "group": 0})
-    for(var i=1; i<arr.length; ++i) {
-        if (arr[i] != "")
-            result.links.push({"source": arr[i], "target": arr[0], "value": 1})
-    }
+    year[arr[0]] = parseInt(arr[1])
 })
 
-rl.on('close', () => {
-    console.log(result)
-    fs.writeFile("output.json", JSON.stringify(result))
+rl_year.on('close', () => {
+    console.log(year)
+
+    //读取结点连接关系
+    const rl_net = readline.createInterface({
+        input: fs.createReadStream("network.txt")
+    });
+
+    rl_net.on('line', (line) => {
+        // console.log('Line from file:' + i + " " + line)
+        // i += 1
+        var arr = line.split("\t")
+        console.log(arr)
+        result.nodes.push({"id": arr[0], "year": year[arr[0]]})
+        for(var i=1; i<arr.length; ++i) {
+            if (arr[i] != "")
+                result.links.push({"source": arr[i], "target": arr[0], "value": 1})
+        }
+    })
+
+    rl_net.on('close', () => {
+        console.log(result)
+        fs.writeFile("output.json", JSON.stringify(result))
+    })
 })
+
+
+
+
