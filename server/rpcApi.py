a655a65a8dc4cb08db5194c0854281c5cb657cb0
lsdlinshunda <923076444@qq.com>
2018-04-21 00:17:21 +0800

    添加种子选取的json-rpc接口

---
+++ server/rpcApi.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+# Shunda Lin 
+# json-rpc api for seed selection
+# 2018.4.20
+from flask import Flask
+from flask_jsonrpc import JSONRPC
+
+# Flask application
+app = Flask(__name__)
+
+# Flask-JSONRPC
+jsonrpc = JSONRPC(app, '/api/algorithm', enable_web_browsable_api=True)
+
+@jsonrpc.method('App.index')
+def index():
+    return 'Welcome to Flask JSON-RPC'
+
+@jsonrpc.method('getSeeds(network=dict, n=int) -> object')
+def getSeeds(network, n):
+    degree = dict()
+    for i in network:
+        degree[i] = len(network[i])
+    # 按出度大小排序
+    degree = sorted(degree, key=degree.__getitem__, reverse=True)
+    seeds = []
+    for i in range(min(n, len(degree))):
+        seeds.append(degree[i])
+    return seeds
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+    
